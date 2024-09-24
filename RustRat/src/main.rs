3e16d83c0fada3b912ad1eae9c6facda5f0cdbf7
affeltrucken <smajlovicaldin52@gmail.com>
2024-09-24 09:19:06 +0200

    Updates

--- RustRat/src/main.rs
+++ RustRat/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512]; // Create a buffer to hold the incoming data
+    match stream.read(&mut buffer) {
+        Ok(size) => {
+            // Print the received data as a string
+            println!("Received: {}", String::from_utf8_lossy(&buffer[..size]));
+        }
+        Err(e) => {
+            println!("Failed to read from stream: {}", e);
+        }
+    }
 }
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:80")?;
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
+}
+
+Expla
